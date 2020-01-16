3f11999136e20aaec81fccd8010285ccdd988a65
Cherry <ex00ia2432@gmail.com>
2020-01-16 17:00:46 +0900

    [create] top

---
+++ components/Top.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="top-wrapper"></div>
+</template>
+
+<script>
+export default {
+  name: 'Top'
+}
+</script>
+
+<style scoped lang="scss">
+.top-wrapper {
+  width: 100vw;
+  height: 1080px;
+  background-color: orange;
+}
+</style>
