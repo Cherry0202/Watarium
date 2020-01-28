1ea1cdcc27ef86a58c3e3f00e8308623ae5c6313
Cherry <ex00ia2432@gmail.com>
2020-01-29 04:04:54 +0900

    [update] about

--- pages/index.vue
+++ pages/index.vue
@@ -16,7 +16,29 @@
       </div>
       <div class="about-wrapper">
         <Title v-bind:titles="titles.about" />
-        <div class="about-content-wrapper"></div>
+        <div class="about-content-wrapper">
+          <div class="about-main-image"></div>
+          <div class="about-sub-content-wrapper">
+            <div class="about-sub-image-wrapper">
+              <div class="about-sub-image"></div>
+            </div>
+            <div class="about-sub-content">
+              <div class="about-sub-content-text">
+                <p>
+                  現代美術を中心に、建築、写真、デザインなど、独自のコンセプトを打ち出している美術館。年に４回の企画展を開催している。併設のアートショップ「オンサンデーズ」には書籍から小物までスタイリッシュなアートグッズが豊富に揃う。
+                </p>
+                <p>
+                  とくにポストカードの品揃えが充実。プレゼント探しにも利用できる。
+                </p>
+                <br />
+                <p>個性的な企画展が魅力の現代美術館</p>
+              </div>
+              <div class="about-sub-content-link">
+                <h2 class="about-link"><a href="#">アクセスはこちら</a></h2>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <div class="exhibition-wrapper">
         <Title v-bind:titles="titles.exhibition" />
@@ -126,12 +148,73 @@ export default {
     .about-wrapper {
       width: 100vw;
       height: 100vh;
-      background-color: orange;
+      /*background-color: orange;*/
       .about-content-wrapper {
         height: 100%;
         width: 90%;
         margin: 0 auto;
-        background-color: lightpink;
+        /*background-color: lightpink;*/
+        display: flex;
+        .about-main-image {
+          height: 80%;
+          width: 45%;
+          /*background-color: lightgoldenrodyellow;*/
+          background-image: url('~assets/images/建物.png');
+          background-size: 100%;
+          background-repeat: no-repeat;
+        }
+        .about-sub-content-wrapper {
+          height: 80%;
+          width: 55%;
+          /*background-color: lightskyblue;*/
+          /*background-size: 100%;*/
+          /*background-repeat: no-repeat;*/
+          .about-sub-image-wrapper {
+            height: 50%;
+            width: 100%;
+            /*background-color: lightcyan;*/
+            .about-sub-image {
+              height: 100%;
+              width: 85%;
+              /*background-color: lightcoral;*/
+              margin: 0 auto;
+              background-image: url('~assets/images/建物２.jpg');
+              background-size: contain;
+            }
+          }
+          .about-sub-content {
+            height: 50%;
+            width: 100%;
+            /*background-color: lightseagreen;*/
+            .about-sub-content-text {
+              height: 75%;
+              width: 85%;
+              margin: 0 auto;
+              /*background-color: lightpink;*/
+              text-align: left;
+              font-size: 20px;
+            }
+            .about-sub-content-link {
+              height: 25%;
+              width: 85%;
+              margin: 0 auto;
+              /*background-color: lightgreen;*/
+              text-align: left;
+              position: relative;
+              .about-link {
+                position: absolute;
+                bottom: 0;
+                a {
+                  text-decoration: none;
+                  font-size: 35px;
+                  margin-top: 10%;
+                  color: #707070;
+                  border-bottom: solid 2px #707070;
+                }
+              }
+            }
+          }
+        }
       }
     }
     .exhibition-wrapper {
